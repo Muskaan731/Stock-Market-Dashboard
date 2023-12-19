096f958fb4e771517ce24f2aca68b0deb04578dd
Muskaan <muskaanshakilshaikh721@gmail.com>
2023-12-19 23:52:34 +0530

    Initial commit: Setup project structure and configuration

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Stock Market Dashboard</h1>
+      <p>This is a simple example. Customize and enhance as needed.</p>
+    </div>
+  );
+};
+
+export default Home;
